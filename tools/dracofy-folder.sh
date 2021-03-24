4fb003b2e49a217d209a0aba054ec7ff70377f9e
Avnerus <avnerus@gmail.com>
2021-03-24 15:30:05 +0200

    fix script

--- tools/dracofy-folder.sh
+++ tools/dracofy-folder.sh
@@ -3,5 +3,5 @@ for file in $1/*
 do
   fbase="$(basename -- $file)"
   echo "$fbase"
-  node bin/3d-tiles-tools.js optimizeB3dm -i "$file" -o "$1/draco/$fbase"
+  node bin/3d-tiles-tools.js dracoCompressB3dm -i "$file" -o "$1/draco/$fbase"
 done
