9110cdd9c4f87124b0aae11aed82b57d4aae38c3
Avnerus <avnerus@gmail.com>
2021-04-30 22:42:41 +0300

    Script arguments update

--- tools/instalod-folder.py
+++ tools/instalod-folder.py
@@ -5,15 +5,18 @@ import argparse
 import os
 import subprocess
 
-parser = argparse.ArgumentParser(description='Draco my Tiles++')
+parser = argparse.ArgumentParser(description='InstaLOD my Tiles++')
 parser.add_argument('input_folder', metavar='input-folder', help='A folder containing .b3dm files')
 parser.add_argument('output_folder', metavar='output-folder', help='A folder where the comprssed tiles will be written')
 parser.add_argument('--concurrency', metavar='N', type=int, default=1, help='Concurrency level')
+parser.add_argument('--blender-path', metavar='path', type=str, default='C:\\Program Files\\Blender Foundation\\Blender 2.92\\blender.exe', help='Path to blender.exe')
+parser.add_argument('--instalod-path', metavar='path', type=str, default='C:\\Apps\\InstaLODPipeline_2020b\\InstaLODCmd.exe', help='Path to InstaLODCmd.exe')
+
     
 args = parser.parse_args()
 
 def process_file(data):
-    input_file, output_file = data
+    input_file, output_file, blender_path, instalod_path = data
     
     if 'lod0' in input_file:
         instlod_profile = 'instalod_2048.json'
@@ -24,6 +27,10 @@ def process_file(data):
                 [
                     'node',
                     'bin/3d-tiles-tools.js',
+                    '-b',
+                    blender_path,
+                    '-l',
+                    instalod_path,
                     '-f', 
                     'instaLODB3dm',
                     '-i', 
@@ -39,6 +46,7 @@ def process_file(data):
 
     if "ERROR" in str(result.stdout):
         print("FAILED: {}".format(input_file))
+        print(result.stdout)
     else:
         print('{} ({})'.format(output_file,instlod_profile))
 
@@ -48,7 +56,7 @@ if __name__ == '__main__':
         if file.endswith(".b3dm"):
             full_path = os.path.join(args.input_folder, file)
             output_path = os.path.join(args.output_folder, file)
-            work.append((full_path, output_path))
+            work.append((full_path, output_path, args.blender_path, args.instalod_path))
 
     print("Running pool with {} workers".format(args.concurrency))
     pool = Pool(args.concurrency)
